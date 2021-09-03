f471e5fc9cbd7158aaafa35d9954b1f0deda6631
SystemTH <89964898+SystemTH@users.noreply.github.com>
2021-09-04 02:11:25 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -18,3 +18,18 @@ echo "${Y}┗━━┓┃  ┗┓┏┛  ┗━━┓┃   ┃┃   ┃┏━━
 echo "${Y}┃┗━┛┃   ┃┃   ┃┗━┛┃   ┃┃   ┃┗━━┓ ┃┃┃┃┃┃      ┃>
 echo "${Y}┗━━━┛   ┗┛   ┗━━━┛   ┗┛   ┗━━━┛ ┗┛┗┛┗┛      ┗>
 echo "                 ${R}YouTube : System TH ${NC}"
+
+echo "select the operation ************"
+echo "  1)operation 1"
+echo "  2)operation 2"
+echo "  3)operation 3"
+echo "  4)operation 4" 
+
+read n
+case $n in
+  1) git clone https://github.com/SystemTH/test.git;;
+  2) echo "You chose Option 2";;
+  3) echo "You chose Option 3";;
+  4) echo "You chose Option 4";;
+  *) echo "invalid option";;
+esac
